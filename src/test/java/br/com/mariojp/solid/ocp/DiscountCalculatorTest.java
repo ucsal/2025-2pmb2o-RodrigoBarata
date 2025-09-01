3d5b11ccb62c2748df46aae6e32449fea3921f1a
Your Name <you@example.com>
2025-09-01 19:33:54 -0300

    Refatoração do DiscountCalculator para utilizar políticas de desconto através de um mapa, permitindo maior flexibilidade na aplicação de descontos por tipo de cliente.

--- src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
+++ src/test/java/br/com/mariojp/solid/ocp/DiscountCalculatorTest.java
@@ -2,11 +2,18 @@ package br.com.mariojp.solid.ocp;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Map;
+import java.util.HashMap;
 
 public class DiscountCalculatorTest {
     @Test
     void partner_gets_12_percent() {
-        var calc = new DiscountCalculator();
+        Map<CustomerType, DiscountPolicy> policies = new HashMap<>();
+        policies.put(CustomerType.REGULAR, new RegularPolicy());
+        policies.put(CustomerType.PREMIUM, new PremiumPolicy());
+        policies.put(CustomerType.PARTNER, new PartnerPolicy());
+        
+        var calc = new DiscountCalculator(policies);
         assertEquals(88.0, calc.apply(100.0, CustomerType.PARTNER), 0.0001,
             "PARTNER deveria ter 12% de desconto");
     }
